76c833ff0911234b7daf2d50b24b275312c23942
ArtSatanov <artsatanov@gmail.com>
2023-08-17 00:48:16 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,35 @@
-import Player from "@vimeo/player";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 console.log(Player);
 
-    const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('play', function () {
+  console.log(timeupdate.seconds);
+});
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
