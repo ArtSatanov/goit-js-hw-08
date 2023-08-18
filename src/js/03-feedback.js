44a44791638c2118f209f6b612ec4fc8f87abbde
Artem Satanovskiy <artsatanov@gmail.com>
2023-08-18 03:36:22 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 import throttle from 'lodash.throttle';
+
+
+const refs = {
+   form: document.querySelector('.feedback-form'),
+   email: document.querySelector('input'),
+   textarea: document.querySelector('textarea'),
+};
+
+console.log(refs.form);
+console.log(refs.email);
+console.log(refs.textarea);
+
+const formData = {};
+
+refs.textarea.addEventListener('input', onTextarea);
+refs.email.addEventListener('input', onTextarea);
+
+
+function onTextarea(e) {
+   const value = e.target.value;
+   // console.log(value);
+   formData.msg = value;
+}
+
+console.log(formData);  
+
+
+
+
+// const formDataSave = function (email, message) { 
+//    const formData = [];
+//    formData.push(email, message);
+//    console.log(formData);
+//    // if (!fromEmailEl.value && ) { }
+//    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+// };
+
+// formEl.addEventListener('input', formDataSave(fromEmailEl.value, fromMsgEl.value));
