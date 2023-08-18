69a7c87ae800a02b032d9cc356e825e93c985610
ArtSatanov <artsatanov@gmail.com>
2023-08-18 17:35:55 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,20 +8,12 @@ const refs = {
 };
 
 const formData = {};
-refs.form.addEventListener('input', onInputEvent);
+
+dataOutput();
+refs.form.addEventListener('input', throttle(onInputEvent, 500));
 refs.form.addEventListener('submit', onSubmit);
 
-// refs.textarea.addEventListener('input', onTextarea);
-// refs.email.addEventListener('input', onTextarea);
-
 function onInputEvent(e) {
-   if (localStorage.getItem('feedback-form-state')) {
-      const savedData = localStorage.getItem('feedback-form-state');
-      const parsedData = JSON.parse(savedData);
-      refs.email.value = formData.email ?? [];
-      refs.textarea.value = formData.message ?? [];
-   }
-
    if (e.target.nodeName === 'INPUT') {
       console.log('Input', e.target.value);
       formData.email = e.target.value;
@@ -38,8 +30,17 @@ function onSubmit(e) {
    const savedData = localStorage.getItem('feedback-form-state');
    const parsedData = JSON.parse(savedData);
    console.log(parsedData);
-   localStorage.removeItem('eedback-form-state');
+   localStorage.removeItem('feedback-form-state');
    e.currentTarget.reset();
+};
+
+function dataOutput() {
+   const savedData = localStorage.getItem('feedback-form-state');
+
+      if (savedData) {
+      const parsedData = JSON.parse(savedData);
+      refs.email.value = parsedData.email || '';
+      refs.textarea.value = parsedData.message || '';
 };
 
 
